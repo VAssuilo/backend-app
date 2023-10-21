35318dad3044ba28fbada1f35ec9b20bfb385ca9
Victor Assuilo <148571833+VAssuilo@users.noreply.github.com>
2023-10-21 16:28:31 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const http = require('http');
 const PORT = 3000;
+const express = require('express');
+const app = express();
+const port = 3001;
+const morgan = require('morgan');
 
+app.use(express.json());
+app.use(morgan('tiny'));
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
@@ -10,3 +16,28 @@ const server = http.createServer((req, res) => {
 server.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
 });
+app.get('/api/persons', (req, res) => {
+    res.json(phonebookData);
+  });
+  let phonebookData = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+];
